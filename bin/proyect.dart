669eaa31a3ece90af909654b8b20f0e78aad0d41
master <dfhavez7@santacecilia.edu.co>
2025-03-31 16:52:06 -0500

    aaaaaaa

---
+++ bin/proyect.dart
@@ -0,0 +1,5 @@
+import 'package:proyect/proyect.dart' as proyect;
+
+void main(List<String> arguments) {
+  print('Hello world: ${proyect.calculate()}!');
+}
---
+++ test/proyect_test.dart
@@ -0,0 +1,8 @@
+import 'package:proyect/proyect.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
